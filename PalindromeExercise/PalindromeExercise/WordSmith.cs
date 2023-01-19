703defab1ee0c355c0d065f716d4f8d768e87004
Alexander Holder <drowish95z@gmail.com>
2023-01-19 11:18:29 -0600

    Ver. #1, 10/10 Passed

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            //Establish baseline
+            int size = word.Length;
+            int checks = 0;
+            bool even = false;
+            char c1 = 'x';
+            char c2 = 'x';
+            //Determine even or odd number of characters
+            if (size % 2 == 0) even = true;
+            //Establish final check
+            if (size <= 0)
+            {
+                return false;
+            } else if (size == 1)
+            {
+                return true;
+            } else if (even == true)
+            {
+                checks = size / 2;
+            } else
+            {
+                checks = (size - 1) / 2;
+            }
+            //Perform final check
+            for (int i = 0; i < checks; i++)
+            {
+                c1 = word[i];
+                c2 = word[size - 1 - i];
+                if (c1 != c2) return false;
+            }
+            return true;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,9 +6,25 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("wow", true)]
+        [InlineData("alex", false)]
+        [InlineData("mom", true)]
+        [InlineData("great", false)]
+        [InlineData("establish", false)]
+        [InlineData("anna", true)]
+        [InlineData("tot", true)]
+        [InlineData("arcane", false)]
+        [InlineData("deed", true)]
+        public void Test1(string word, bool expected)
         {
+            //Arrange
+            var smith = new WordSmith();
+            //Act
+            var actual = smith.IsAPalindrome(word);
+            //Assert
+            Assert.Equal(expected, actual);
 
         }
     }
